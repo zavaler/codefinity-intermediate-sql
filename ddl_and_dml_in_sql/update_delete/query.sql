8e86ab53ea8219c935dd31c112671037c73c0028
zavaler <>
2025-11-02 13:59:52 +0000

    Update & Delete

---
+++ ddl_and_dml_in_sql/update_delete/query.sql
@@ -0,0 +1,9 @@
+Update medications
+    Set price = 10
+    Where price < 10;
+
+
+-- Please do not modify the code below!
+SELECT * FROM medications
+ORDER BY id;
+-- Please do not modify the code above!
